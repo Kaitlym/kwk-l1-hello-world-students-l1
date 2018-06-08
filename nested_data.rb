87e13de25281b4e156e9cab29f3beb4dbabd5b8f
Kaitlym <azkatie2014@gmail.com>
2018-06-08 18:59:12 +0000

    automatically backed up by learn

--- nested_data.rb
+++
@@ -1,8 +0,0 @@
-group_name = {
-  :kaitlyn => ["ligh em up", "the wolf"],
-  :alex => ["green light", "location"],
-  :baylee => ["want you back", "something to tell you"]
-}
-puts "kaitlyn's favorite songs are #{group_name [:kaitlyn]}"
-puts "alex's favorite songs are #{group_name [:alex][0]}"
-puts " baylee's favorite songs are #{group_name [:baylee][0]}"
