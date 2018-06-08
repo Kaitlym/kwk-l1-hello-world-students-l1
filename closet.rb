05d628bdce6f8da270247432c1c2d9beec061042
Kaitlym <azkatie2014@gmail.com>
2018-06-08 18:15:54 +0000

    automatically backed up by learn

--- closet.rb
+++ closet.rb
@@ -6,5 +6,7 @@ closet = [
   puts closet[0][0]
   
   closet2 = {
-    :tops => ["blouse", "sweater", "crop"] #nested array
+    :tops => ["blouse", "sweater", "crop"] #nested array #tops
+    :pants =>  ["jeans", "shorts", "skirt"], #pants
+    :shoes =>["running", "heel", "sandals"] # shoes
   }
