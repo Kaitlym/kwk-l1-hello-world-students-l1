c8928532c1b9bc67a40d4216c0a20e1b353989c6
Kaitlym <azkatie2014@gmail.com>
2018-06-08 18:55:36 +0000

    automatically backed up by learn

--- nested_data.rb
+++ nested_data.rb
@@ -1,5 +1,8 @@
 group_name{
-  :kaitlyn ["ligh em up", ]
-  :person_2 []
-  :person_3 []
+  :kaitlyn ["ligh em up", "the wolf"],
+  :alex ["green light", "location"],
+  :baylee ["want you back", "something to tell you"]
 }
+puts "kaitlyn's favorite songs are #{group_name [:kaitlyn][0]}
+group_name [:alex][0]
+group_name [:baylee][0]
